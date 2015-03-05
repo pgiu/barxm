a0935a71b2551d52b0819faf94973a9af4b22a36
Pablo <pablogiudice@gmail.com>
2015-03-05 15:35:11 -0300

    add junit tests

---
+++ src/test/java/com/pgiu/BarxmTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pgiu;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pgiudice
+ */
+public class BarxmTest {
+    
+    public BarxmTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void test1(){
+    assertEquals("10 x 0 must be 0", 0,0);
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
